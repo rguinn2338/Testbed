8216f2adb471ea4755ea3af08663bb920c404ae6
Ryan Guinn <RGuinn2338@gmail.com>
2014-12-13 00:10:52 -0600

    added files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,11 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int var = 3;
+		var++;
+		
 		System.out.println("yo sup");
+		System.out.println("number:" + var);
 	}
 
 }
